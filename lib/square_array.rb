d4ebf262fcacb9ac1d477a72b6c943deea624362
Mackenzie Pech <mackenzieerinpech@gmail.com>
2019-06-22 18:17:08 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,10 +1,9 @@
 def square_array(array)
   squared = []	  squared = []
-  array.each { |element| squared << element ** 2 }	  
-  count = 0
+  array.each { |element| squared << element ** 2 }	  count = 0
   while count < array.size do
     squared << (array[count] ** 2)
     count = count + 1
   end
-  squared	  squared
+  squared
 end
